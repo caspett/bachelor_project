d0e23b4e0b83e101d00a47c323a3afb438f5021d
Casper Pettersen <casper.pettersen@bdo.no>
2023-02-14 09:19:25 +0100

    Updated container block parameters

--- non-confidential/deployments.tf
+++ non-confidential/deployments.tf
@@ -25,25 +25,26 @@ resource "kubernetes_deployment" "nginx" {
       spec {
         container {
           name  = "example"
-          image = "nginx:1.7.8"
-          env = [
-            {
+          # image = "nginx:1.7.8"
+          image = "../docker/bachelor_img.tar"
+          image_pull_policy = "Never"
+          env {
               name  = "DB_HOST"
               value = azurerm_mssql_server.example.fully_qualified_domain_name
-            },
-            {
+            }
+
+          env {
               name  = "DB_USER"
               value = azurerm_mssql_server.example.administrator_login
-            },
-            {
+            }
+          env {
               name  = "DB_PASS"
               value = azurerm_mssql_server.example.administrator_login_password
-            },
-            {
+            }
+          env {
               name  = "DB_NAME"
               value = azurerm_mssql_database.test.name
             }
-          ]
           port {
             container_port = 80
           }
