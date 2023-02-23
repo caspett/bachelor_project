bd6b97bd46ae0fe72cb14bba708fd67fff7a1e99
Casper Pettersen <casper.pettersen@bdo.no>
2023-02-23 11:55:24 +0100

    Removed test code

--- docker/index-test.php
+++ docker/index-test.php
@@ -14,7 +14,6 @@
     $options = array(
         PDO::MYSQL_ATTR_SSL_CA => "/var/www/html/BaltimoreCyberTrustRoot.crt.pem"
     );
-    // $db = new PDO("mysql:host=" . $host . ";port=3306;dbname=" . $db_name, $username, $password, $options);   
     $connection = null;
     try{
         $connection = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password, $options);
@@ -22,14 +21,6 @@
         echo "Hello";
     }catch(PDOException $exception){
         echo "Connection error: " . $exception->getMessage();
-    }
-
-    try{
-        $ssl_connection = "SELECT * FROM performance_schema.session_status WHERE VARIABLE_NAME IN ('Ssl_version','Ssl_cipher');";
-        $result = $connection->query($ssl_connection) ;
-        echo $result;
-    }catch(PDOException $exception) {
-        echo "Query error: " . $exception->getMessage();
     }
 
     // Execute a simple MySQL query to retrieve some data
