7da747867ddc38077a8b438376f2994ac45a2526
Markus Strømme <markusstroemme@gmail.com>
2023-03-07 09:28:29 +0100

    Lagde index-combined som utnytter både index-heavy, og index-test

--- docker/index-test.php
+++ docker/index-test.php
@@ -11,12 +11,13 @@
     $db_name = getenv('DB_NAME');
     $username = getenv('DB_USER');
     $password = getenv('DB_PASS');
-    $options = array(
-        PDO::MYSQL_ATTR_SSL_CA => "/var/www/html/BaltimoreCyberTrustRoot.crt.pem"
-    );
+    // $options = array(
+    //     PDO::MYSQL_ATTR_SSL_CA => "/var/www/html/BaltimoreCyberTrustRoot.crt.pem"
+    // );
     $connection = null;
     try{
-        $connection = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password, $options);
+        // $connection = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password, $options);
+        $connection = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
         $connection->exec("set names utf8");
         echo "Hello";
     }catch(PDOException $exception){
