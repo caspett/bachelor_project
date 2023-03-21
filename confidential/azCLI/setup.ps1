c69d38dcee5152918358cfc4c36c42d2dddb5572
Casper Pettersen <casperp@stud.ntnu.no>
2023-03-21 22:13:18 +0100

    Changed more code

--- confidential/azCLI/setup.ps1
+++ confidential/azCLI/setup.ps1
@@ -1,26 +1,7 @@
-## Set up an SGX-capable cluster using the following commands:
-
-# Create a resource group
-az group create --name rg-confidential-dev --location northeurope
-
-# Create an SGX capable cluster with a Confidential Computing addon
-az aks create -l northeurope -g rg-confidential-dev -n conf-arck-web-cluster --vm-set-type VirtualMachineScaleSets --network-plugin azure --node-count 1 --node-vm-size Standard_DC2as_v5 --aks-custom-headers usegen2vm=true --enable-sgxquotehelper --enable-addon confcom --generate-ssh-keys
-
-# Get the Kubernetes credentials. This will store the credentials in your .kube/config file
-az aks get-credentials --admin --name conf-arck-web-cluster --resource-group rg-confidential-dev --overwrite-existing
 
 
-## Use the following commands to verify that the nodes are created properly and the SGX-related DaemonSets are running on DCsv2 node pools.
-kubectl get nodes -o wide
-kubectl get pods --all-namespaces
-
-
-
-
-
-
-
-
+#Get cluster credentials
+az aks get-credentials --resource-group rg-confidential-dev --name conf-arck-web-cluster
 
 ## Retrieve the join token for your Fortanix CCM account from the CCM UI and store it as a Kubernetes secret in your cluster.
 # Navigate to the COMPUTE NODES tab in the CCM UI and click + ENROLL NODE to bring up the token dialog. Copy the token into the following variable
@@ -29,13 +10,26 @@ $token = "MX1emNUDwnpWNXpjf0ZEVVJfkwtq67Vj9M6nhC8M4Xqs31Tizkcynnhz74uXttEPI1CPAh
 # Use the following command to store the token as a Kubernetes secret for the cluster (Replace <token> value with your token)
 kubectl create secret generic em-token --from-literal=token=$token
 
-## Deploy the node agent DaemonSet using the CCM node agent YAML file below
-
-# Fortanix CCM node agent YAML file:
-See own yaml file
-
 # Deploy the node agent DaemonSet
 kubectl create -f agent-daemonset.yaml
 
 ## The CCM node agent DaemonSets are now deployed. Validate that the node agent pod is up and running using the command
 kubectl get pods --all-namespaces
+
+#Deploy application and load balancer:
+kubectl apply -f conf-webserver.yaml
+
+#Check application logs:
+kubectl logs -f sgx-test-m5ck9
+
+kubectl logs -l app=sgx-test
+
+
+
+#Cleanup
+
+#Destroy load balancer
+kubectl delete service sgx-test-lb  
+
+#Destroy application
+kubectl delete job sgx-test
--- confidential/infrastructure/cluster.tf
+++ confidential/infrastructure/cluster.tf
@@ -15,8 +15,8 @@ resource "azurerm_kubernetes_cluster" "this" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    # vm_size    = "Standard_DC2ds_v3"
-    vm_size    = "Standard_DC4s_v2"
+    vm_size    = "Standard_DC2ds_v3"
+    # vm_size    = "Standard_DC4s_v2"
 
   }
 
@@ -36,6 +36,20 @@ resource "null_resource" "enable_addons" {
   provisioner "local-exec" {
     command = "az aks enable-addons --addons confcom --name ${azurerm_kubernetes_cluster.this.name} --resource-group ${var.target_resource_group}"
   }
+}
+
+# Get containter registry information
+data "azurerm_container_registry" "this" {
+  name                = "KubDocker"
+  resource_group_name = "rg-non-confidential-dev"
+}
+
+#Role assigment for allowing cluster to pull image from container registry
+resource "azurerm_role_assignment" "this" {
+  principal_id                     = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = data.azurerm_container_registry.this.id
+  skip_service_principal_aad_check = true
 }
 
 output "client_certificate" {
