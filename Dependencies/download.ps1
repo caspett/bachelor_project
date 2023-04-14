11d6797c0463aa7599a72d57a4dedf5aa0b16a1b
caspett <83064911+caspett@users.noreply.github.com>
2023-04-14 14:34:27 +0200

    Delete Dependencies directory

--- Dependencies/download.ps1
+++
@@ -1,8 +0,0 @@
-# Install Chocolatey
-if (!(Get-Command choco.exe -ErrorAction SilentlyContinue)) {
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-}
-
-# Install MySQL, Git, Terraform and Azure CLI using Chocolatey
-choco install mysql git terraform azure-cli kubernetes-cli -y
